468a4d7c330b9ecf731642eecc2cfc173edac195
dkuska <70569959+dkuska@users.noreply.github.com>
2022-12-15 11:56:43 +0100

    Added comments and type hints

--- examples/model/nodes.py
+++ examples/model/nodes.py
@@ -12,6 +12,7 @@ class Node(ABC):
 
 @dataclass
 class PandasNode(Node):
+    # TODO: Figure out what information we need to save
     pass
 
 
@@ -34,19 +35,19 @@ class SQLNode(PandasNode):
         self.con = self.extract_con(origin)
         self.index = self.extract_index(origin)
 
-    def extract_sql_str(self, origin: cst.CSTNode):
-        return ""  # TODO: Implement
+    def extract_sql_str(self, origin: cst.CSTNode) -> str:
+        return ""  # TODO: Implement by traversing origin
 
-    def extract_con(self, origin: cst.CSTNode):
-        return ""
+    def extract_con(self, origin: cst.CSTNode) -> str:
+        return "" # TODO: Implement by traversing origin
 
-    def extract_index(self, origin: cst.CSTNode):
-        return ""
+    def extract_index(self, origin: cst.CSTNode) -> str:
+        return "" # TODO: Implement by traversing origin
 
 
 @dataclass
 class JoinNode(PandasNode):
-    parents: Optional[list[cst.CSTNode]] = None
+    parents: Optional[list[cst.CSTNode]] = None #TODO: Figure out what this could be used for...
     left: Optional[SQLNode] = None
     right: Optional[SQLNode] = None
     on: Optional[tuple[str]] = None
@@ -56,9 +57,9 @@ class JoinNode(PandasNode):
     sort: Optional[bool] = False
     validate: Optional[str] = ""
 
-    def __init__(self, origin):
+    def __init__(self, origin, parents = None):
         self.origin = origin
-
+        self.parents = parents
         self.left, self.right = self.extract_join_partners(origin)
         self.on = self.extract_on(origin)
         self.how = self.extract_how(origin)
@@ -66,24 +67,27 @@ class JoinNode(PandasNode):
         self.sort = self.extract_sort(origin)
         self.validate = self.extract_validate(origin)
 
-    def extract_join_partners(self, origin: cst.CSTNode):
-        pass
+    def extract_join_partners(self, origin: cst.CSTNode) -> tuple[cst.CSTNode, cst.CSTNode]:
+        pass # TODO: Implement by traversing origin, will also need additional information about all other nodes
 
-    def extract_on(self, origin: cst.CSTNode):
-        pass
+    def extract_on(self, origin: cst.CSTNode) -> str:
+        pass # TODO: Implement by traversing origin
 
-    def extract_how(self, origin: cst.CSTNode):
-        pass
+    def extract_how(self, origin: cst.CSTNode) -> str:
+        pass # TODO: Implement by traversing origin
 
-    def extract_suffix(self, origin: cst.CSTNode):
-        pass
+    def extract_suffix(self, origin: cst.CSTNode) -> tuple[str, str]:
+        pass # TODO: Implement by traversing origin
 
-    def extract_sort(self, origin: cst.CSTNode):
-        pass
+    def extract_sort(self, origin: cst.CSTNode) -> bool:
+        pass # TODO: Implement by traversing origin
 
-    def extract_validate(self, origin: cst.CSTNode):
-        pass
+    def extract_validate(self, origin: cst.CSTNode) -> str:
+        pass # TODO: Implement by traversing origin
 
+
+    def create_sql_query(self) -> str:
+        return ''
 
 @dataclass
 class AggregationNode(PandasNode):
