134f2684e2a8f01c2b880cded0745859b53bb261
dkuska <70569959+dkuska@users.noreply.github.com>
2023-01-12 20:08:08 +0100

    WIP: First Commit

---
+++ src/cst/NodeReplacer.py
@@ -0,0 +1,36 @@
+import libcst as cst
+from collections import OrderedDict
+
+class NodeReplacer():
+    """
+    The function of this is to replace Nodes in the old CST with the new nodes obtained through our optimizations.
+    """
+    def __init__(self) -> None:
+        pass
+    
+    def replace(self, src_tree: cst.Module, old_new_nodes: OrderedDict) -> cst.Module:
+        new_tree_body = []
+        # Iterate over old CST
+        for src_node in src_tree.body:
+            node_replaced = False
+            node_replacement = None
+            
+            # TODO: This is a quick fix, investigate if src_node.body can contain multiple statements...
+            if src_node.body[0] in old_new_nodes:
+                node_replaced = True
+                node_replacement = old_new_nodes[src_node.body[0]]
+                
+                if node_replacement is None: # Also a quick fix...
+                    node_replaced = False
+                    
+            if node_replaced:
+                new_tree_body.append(node_replacement)
+            else:
+                new_tree_body.append(src_node)
+    
+        return cst.Module(body=new_tree_body, 
+                          header=src_tree.header, footer=src_tree.footer,
+                          encoding=src_tree.encoding, 
+                          default_indent=src_tree.default_indent,
+                          default_newline=src_tree.default_newline,
+                          has_trailing_newline=src_tree.has_trailing_newline)
---
+++ src/model/nodes.py
@@ -0,0 +1,60 @@
+from abc import ABC
+
+from libcst import CSTNode
+
+
+class IRNode(ABC):
+    def __init__(self, parent=None, library=None, *args, **kwargs):
+        self.parent = parent
+        self.library = library
+        if args or kwargs:
+            print(f"These args: {args} and kwargs: {list(kwargs.keys())} don't have a translation rule yet")
+
+
+class DataFrameNode(IRNode):
+    pass
+
+
+class SQLNode(DataFrameNode):
+    def __init__(self, sql: CSTNode, con, *args, **kwargs):
+        self.sql = sql
+        self.con = con
+
+        super().__init__(*args, **kwargs)
+
+    @property
+    def sql_string(self):
+        return self.sql.value.replace('"', "")
+
+    def to_code(self):
+        return f"read_sql({self.sql_string}, SOME CON)"
+
+
+class JoinNode(DataFrameNode):
+    def __init__(self, left: DataFrameNode, right: DataFrameNode, *args, **kwargs):
+        left.parent = self
+        right.parent = self
+
+        self.left = left
+        self.right = right
+
+        super().__init__(*args, **kwargs)
+
+    def to_code(self):
+        if hasattr(self.left, "sql_string") and hasattr(self.right, "sql_string"):
+            return f"read_sql('SELECT * FROM ({self.left.sql_string}) JOIN ({self.right.sql_string})')"
+        else:
+            return f"({self.left.to_code()}).join({self.right.to_code()})"
+
+
+class SetKeyNode(DataFrameNode):
+    def __init__(self, node: DataFrameNode, key: CSTNode, *args, **kwargs):
+        node.parent = self
+
+        self.node = node
+        self.key = key
+
+        super().__init__(*args, **kwargs)
+
+    def to_code(self):
+        return f"({self.node.to_code()}).set_key({self.key.value})"
