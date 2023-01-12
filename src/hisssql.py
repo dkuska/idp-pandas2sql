65cb14c78e7ec6ec384ea11436a16ec566be2a90
dkuska <70569959+dkuska@users.noreply.github.com>
2023-01-12 21:34:19 +0100

    Cleanup before PR

--- src/hisssql.py
+++ src/hisssql.py
@@ -16,6 +16,7 @@ def write_new_file_contents(new_file_path: str, new_contents):
 def parse_args():
     parser = ArgumentParser(description="Optimize SQL based Pandas operations for Joins and Aggregations")
     parser.add_argument("filepath", type=str, help="Path to the file you want to translate")
+    # TODO: Implement these
     parser.add_argument("--inplace", type=bool, default=True, help="Whether or not to replace the file contents inplace or ")
     parser.add_argument("--new-filepath", type=str, help='Path of new file if --inplace==False')
     args = parser.parse_args()
@@ -26,17 +27,16 @@ def main():
     args = parse_args()
 
     src = parse_file_contents(args.filepath)
-    print("Old Source:")
+    print("Old Source:")  # DEBUG
     print(src)
 
-    orchestrator = Orchestrator()
-
-    new_src = orchestrator.transform(src)
-    print("New Source:")
+    new_src = Orchestrator.transform(src)
+    
+    print("New Source:")  # DEBUG
     print(new_src)
     if args.inplace:
         new_file_path = args.filepath
-    else:
+    else: # TODO: Actually implement this
         new_file_path = args.filepath
         
     write_new_file_contents(new_file_path, new_src)
