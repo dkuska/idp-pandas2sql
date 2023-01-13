bd9cd94b5e292d804542588b2b05a61e724ff4d8
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-01-13 19:20:22 +0100

    Implement aggregation on select *

--- examples/libcst_example_ir.py
+++ examples/libcst_example_ir.py
@@ -11,10 +11,14 @@ from pandas import read_sql
 con1 = blub()
 
 df1 = read_sql("SELECT * FROM table1", con1)
-df2 = pd.read_sql(sql="SELECT * FROM table2", con=con1)
+df2 = pd.read_sql(sql="SELECT key1, key2 FROM table2", con=con1)
 
 df3 = df1.join(df2, how='inner')
 df4 = df1.set_index('key').join(df2.set_index('key'))
+
+sum = df2.sum()
+max = df1.max()
+
 
 """
 
@@ -29,7 +33,7 @@ def main():
 
     # nodes contain all visited statements in order
     for variable, node in node_selector.variables.items():
-        print(variable, type(node), end=" ")
+        print(variable, node.__class__.__name__, end=" ")
         if isinstance(node, DataFrameNode):
             print(node.to_code())
         else:
