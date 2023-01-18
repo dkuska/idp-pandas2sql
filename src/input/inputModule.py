ff896f13c69166b8b36d9fde6224822164479c68
Tobias Sträubig <tobi_2001@hotmail.de>
2023-01-18 22:55:52 +0100

    split lib method handlers into different methods

--- src/input/inputModule.py
+++ src/input/inputModule.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from model.nodes import IRNode
 
 
@@ -11,8 +13,34 @@ class InputModule:
         """Return every symbol that is imported when importing * from the library"""
         return []
 
-    def visit_call(self, func_name: str, args: list, kwargs: dict) -> IRNode:
-        pass
+    def visit_method(self, func_name: str, args: list, kwargs: dict) -> Optional[IRNode]:
+        visitor_method_name = f"visit_{func_name}"
 
-    def visit_call_on_ir_node(self, ir_node: IRNode, func_name: str, args: list, kwargs: dict) -> IRNode:
-        pass
+        if not hasattr(self, visitor_method_name):
+            print(f'Warning: Call of method "{func_name}" of library "{self.module_name}" has no handler.')
+            return None
+
+        visitor_method = getattr(self, visitor_method_name)
+        result = visitor_method(*args, **kwargs)
+        if not result:
+            print(f'Warning: Call of method "{func_name}" of library "{self.module_name}" returns no IR.')
+            return None
+        if not result.library:
+            result.library = self.module_name
+        return result
+
+    def visit_df_method(self, ir_node: IRNode, func_name: str, args: list, kwargs: dict) -> Optional[IRNode]:
+        visitor_method_name = f"visit_df_{func_name}"
+
+        if not hasattr(self, visitor_method_name):
+            print(f'Warning: Call of method "{func_name}" on dataframe of library "{self.module_name}" has no handler.')
+            return None
+
+        visitor_method = getattr(self, visitor_method_name)
+        result = visitor_method(ir_node, *args, **kwargs)
+        if not result:
+            print(f'Warning: Call of method "{func_name}" on dataframe of library "{self.module_name}" returns no IR.')
+            return None
+        if not result.library:
+            result.library = self.module_name
+        return result
