c20cd213e5985dce492851a3d06de20f6d9b120e
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-01-19 16:42:30 +0100

    Add and improve types in dask input module

--- src/input/dask.py
+++ src/input/dask.py
@@ -1,5 +1,5 @@
 from input.inputModule import InputModule
-from model.nodes import IRNode, JoinNode, SQLNode
+from model.nodes import DataFrameNode, JoinNode, SQLNode
 
 
 class DaskInput(InputModule):
@@ -12,10 +12,10 @@ class DaskInput(InputModule):
     def visit_read_sql_query(self, *args, **kwargs):
         return SQLNode(*args, **kwargs)
 
-    def visit_read_sql_table(self, table_name, *args, **kwargs):
+    def visit_read_sql_table(self, table_name: str, *args, **kwargs):
         return SQLNode(f"SELECT * FROM {table_name}", *args, **kwargs)
 
     # df methods
 
-    def visit_df_merge(self, ir_node: IRNode, *args, **kwargs):
-        return JoinNode(ir_node, *args, **kwargs)
+    def visit_df_merge(self, df_node: DataFrameNode, *args, **kwargs):
+        return JoinNode(df_node, *args, **kwargs)
