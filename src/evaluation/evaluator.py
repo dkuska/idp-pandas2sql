c5eb12f0813eb93f4252c74b549c95c21ecb5d7f
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-01-24 11:52:23 +0100

    Fix and improve evaluator

--- src/evaluation/evaluator.py
+++ src/evaluation/evaluator.py
@@ -1,12 +1,12 @@
 import time
 import warnings
 
-from db import LOCAL_CONFIG_01GB
-from pipeline_examples import PipelineCode, PipelineExample
+from .db import LOCAL_CONFIG_1GB
+from .pipeline_examples import PipelineCode, PipelineExample
 
 warnings.filterwarnings("ignore")
 
-DB_CONFIG = LOCAL_CONFIG_01GB
+DB_CONFIG = LOCAL_CONFIG_1GB
 
 
 class Evaluator:
@@ -17,11 +17,11 @@ class Evaluator:
 
     def evaluate_function(self, code: PipelineCode) -> float:
         start = time.time()
-        eval(code)
+        exec(code)
         end = time.time()
         execution_time = end - start
 
-        return execution_time  # TODO change to seconds
+        return execution_time
 
     def evaluate(self):
         print(f"Evaluating {self.pipeline.name.upper()}:")
@@ -36,20 +36,20 @@ lineitem_join_orders_pipeline = PipelineExample(
     "Lineitems Join Orders",
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
         line_items = pandas.read_sql("SELECT l_orderkey, l_quantity FROM lineitem", conn)  # 13.25s 96MB
         orders = pandas.read_sql("SELECT o_orderkey, o_totalprice FROM orders", conn)  # 3.21 24MB
         results = pandas.merge(
             line_items, orders, left_on="l_orderkey", right_on="o_orderkey", how="left"
         )  # 1.06s 240MB
-        # return pandas.merge(line_items, orders, on="orderkey", suffixes=("l_", "o_"), how="left")
+        # results = pandas.merge(line_items, orders, on="orderkey", suffixes=("l_", "o_"), how="left")
     """,
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
-        return pandas.read_sql(
+        results = pandas.read_sql(
             "SELECT * FROM \
             (SELECT l_orderkey, l_quantity FROM lineitem) AS t1\
             LEFT JOIN (SELECT o_orderkey, o_totalprice FROM orders) AS t2\
@@ -64,16 +64,16 @@ partsupp_join_parts_pipeline = PipelineExample(
     "Partsupp Join Parts",
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
         partsupps = pandas.read_sql("SELECT * FROM partsupp", conn)
         parts = pandas.read_sql("SELECT * FROM part", conn)
         result = pandas.merge(partsupps, parts, left_on="ps_partkey", right_on="p_partkey", how="left")
-        # return pandas.merge(line_items, orders, on="orderkey", suffixes=("l_", "o_"), how="left")
+        # results = pandas.merge(line_items, orders, on="orderkey", suffixes=("l_", "o_"), how="left")
     """,
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
         result = pandas.read_sql(
             "SELECT * FROM \
@@ -90,14 +90,14 @@ max_lineitem_discount_pipeline = PipelineExample(
     "Max Discount of Lineitems",
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
         line_items_discounts = pandas.read_sql("SELECT l_discount FROM lineitem", conn)
         result = line_items_discounts.max(axis=0)[0]
     """,
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
         result = pandas.read_sql("SELECT MAX(l_discount) as max FROM lineitem", conn)["max"][0]
     """,
@@ -108,16 +108,16 @@ sum_orders_totalprice_pipeline = PipelineExample(
     "Sum Totalprice of Orders",
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
         line_items_discounts = pandas.read_sql("SELECT o_totalprice FROM orders", conn)
-        return line_items_discounts.sum(axis=0)[0]
+        results = line_items_discounts.sum(axis=0)[0]
     """,
     """
     import pandas
-    from db import PostgresConnection
+    from .db import PostgresConnection
     with PostgresConnection(DB_CONFIG) as conn:
-        return pandas.read_sql("SELECT SUM(o_totalprice) FROM orders", conn)["sum"][0]
+        results = pandas.read_sql("SELECT SUM(o_totalprice) FROM orders", conn)["sum"][0]
     """,
 )
 
