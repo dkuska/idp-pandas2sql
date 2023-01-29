25aa19d61ae6bf8025907bd1e916bf2809224e2c
AbdullatifGhajar <abdullatif.ghajar@gmail.com>
2023-01-29 23:30:13 +0100

    Use signatures instead of kwargs.get

--- src/code_translation/input/pandas.py
+++ src/code_translation/input/pandas.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional, Union
 
 import libcst as cst
 
@@ -32,16 +32,55 @@ class PandasInput(InputModule):
     def visit_df_sort_values(self, df_node: DataFrameNode, *args, **kwargs):
         return SortNode(df_node, *args, **kwargs)
 
-    def visit_df_join(self, df_node: DataFrameNode, *args, **kwargs):
-        # TODO This is only if sort is kwargs. Handle if sort is in *args
-        if kwargs.get("sort", False):
-            return SortNode(JoinNode(df_node, *args, **kwargs))
-        return JoinNode(df_node, *args, **kwargs)
+    def visit_df_join(
+        self,
+        df_node: DataFrameNode,
+        other: DataFrameNode,
+        on: Optional[str] = None,
+        how: Optional[str] = None,
+        lsuffix="",
+        rsuffix="",
+        sort=False,
+        validate: Optional[bool] = None,
+    ):
+        join_node = JoinNode(df_node, other, on=on, how=how, lsuffix=lsuffix, rsuffix=rsuffix, validate=validate)
+        if sort:
+            return SortNode(join_node)
+        return join_node
 
-    def visit_df_merge(self, df_node: DataFrameNode, *args, **kwargs):
-        if kwargs.get("sort", False):
-            return SortNode(JoinNode(df_node, *args, **kwargs))
-        return JoinNode(df_node, *args, **kwargs)
+    def visit_df_merge(
+        self,
+        df_node: DataFrameNode,
+        right: DataFrameNode,
+        how: Optional[str] = None,
+        on: Optional[str] = None,
+        left_on: Optional[str] = None,
+        right_on: Optional[str] = None,
+        left_index=False,
+        right_index=False,
+        sort=False,
+        suffixes=("_x", "_y"),
+        copy=True,
+        indicator=False,
+        validate: Optional[bool] = None,
+    ):
+        join_node = JoinNode(
+            df_node,
+            right,
+            on=on,
+            how=how,
+            left_on=left_on,
+            right_on=right_on,
+            left_index=left_index,
+            right_index=right_index,
+            suffixes=suffixes,
+            copy=copy,
+            indicator=indicator,
+            validate=validate,
+        )
+        if sort:
+            return SortNode(join_node)
+        return join_node
 
     def visit_df_set_index(self, df_node: DataFrameNode, *args, **kwargs):
         return SetKeyNode(df_node, *args, **kwargs)
