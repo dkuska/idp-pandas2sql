978d70542a7742be26d5660027d068e831b5d318
dkuska <70569959+dkuska@users.noreply.github.com>
2023-01-31 21:27:14 +0100

    Fixed tests

--- test/test_pandas_pipelines.py
+++ test/test_pandas_pipelines.py
@@ -3,7 +3,7 @@ from src.evaluation.examples import (
     PipelineExample,
     aggregation_pipeline_examples,
     join_pipeline_examples,
-    operator_chaining_pipeline_examples,
+    operator_chaining_examples,
     sort_pipeline_examples,
 )
 
@@ -36,7 +36,7 @@ def test_aggregation_pipeline(pipeline: PipelineExample):
 
 @given(
     "pipeline",
-    operator_chaining_pipeline_examples,
+    operator_chaining_examples,
 )
 def test_operator_chaining_pipeline(pipeline: PipelineExample):
     assert_equal_code(Orchestrator().transform(pipeline.code), pipeline.optimized_code)
